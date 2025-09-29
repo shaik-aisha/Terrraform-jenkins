90fe26afba679a138c9eeb79577a6faf47bfbca9
shaik-aisha <58479027+shaik-aisha@users.noreply.github.com>
2025-09-29 14:44:26 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+# variables.tf
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "Name tag for EC2 instance"
+  default     = "MyFreeTierEC2"
+}
