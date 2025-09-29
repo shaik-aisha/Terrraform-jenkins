46cecdb89994ae0c02103061e41a1ff0a0a2bd18
shaik-aisha <58479027+shaik-aisha@users.noreply.github.com>
2025-09-29 15:01:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# EC2 instance
 resource "aws_instance" "assath" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
