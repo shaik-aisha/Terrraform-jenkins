bd3fe3da1adc289db4d956d5bc049411d3a39279
shaik-aisha <58479027+shaik-aisha@users.noreply.github.com>
2025-09-29 14:34:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "assath" {
   ami           = "ami-01b6d88af12965bb6"  # Amazon Linux 2
-  instance_type = "t2.micro"                # Change to Free Tier
+  instance_type = "t3.micro"                # Change to Free Tier
   tags = {
     Name = "MyGitactionsEC2"
   }
